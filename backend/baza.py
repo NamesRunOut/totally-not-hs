627e84e6f73443251ffcbb00c16b7cc4635b3704
pawel200020 <pawel200020@gmail.com>
2023-01-12 23:24:05 +0100

    merge backend

--- backend/baza.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('db.sqlite3')
-c = conn.cursor()
-
-c.execute('''CREATE TABLE Player (id INTEGER, points INTEGER, name TEXT)''')
-
-conn.commit()
-conn.close
--- backend_3_0/Card.py
+++
@@ -1,8 +0,0 @@
-class Card(object):
-    def __init__(self, id, name, description, mana, hp, atk):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.mana = mana
-        self.hp = hp
-        self.atk = atk
